69b916355c96a17de3c5ef6637ba3a84ae1a59dd
Lukas Szimtenings <lszimtenings@ukaachen.de>
2021-05-11 15:22:18 +0200

    setup basic reddit_cralwer

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 Uhhhh, this is a fucking banger.
+
+# Setup
+## Reddit
+* rename or copy ``./res/reddit_config.json.example`` to ``./res/reddit_config.json``
+* Open ``./res/reddit_config.json`` and enter all fields with the right information from https://old.reddit.com/prefs/apps
---
+++ retrieval/Reddit.py
@@ -0,0 +1,14 @@
+import praw
+import json
+
+config_path = "res/reddit_config.json"
+if __name__ == '__main__':
+    config_path = "../res/reddit_config.json"
+
+with open(config_path, "r", encoding="UTF-8") as config_file:
+    config = json.load(config_file)
+
+
+reddit = praw.Reddit(client_id=config["client_id"], client_secret=config["client_secret"],
+                     password=config["password"], user_agent="android:com.example.myredditapp:v1.2.4",
+                     username=config["username"])
