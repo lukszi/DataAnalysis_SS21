be666c49896d7e6f7e0de228086aaac192714235
Lukas Szimtenings <lszimtenings@ukaachen.de>
2021-05-11 15:27:11 +0200

    crawler now outputs the 256 newest post scores

--- retrieval/Reddit.py
+++ retrieval/Reddit.py
@@ -12,3 +12,8 @@ with open(config_path, "r", encoding="UTF-8") as config_file:
 reddit = praw.Reddit(client_id=config["client_id"], client_secret=config["client_secret"],
                      password=config["password"], user_agent="android:com.example.myredditapp:v1.2.4",
                      username=config["username"])
+
+wsb = reddit.subreddit("WallStreetBets")
+wsb_new = wsb.new(limit=256)
+for submission in wsb_new:
+    print(submission.score)
