bd1fcae8c340c2c1a68f6da41da9512bbdf9dc83
Lukas Szimtenings <lszimtenings@ukaachen.de>
2021-06-22 15:36:21 +0200

    refactor and write basic first symbol extraction

--- retrieval/Reddit/Reddit.py
+++ retrieval/Reddit/Reddit.py
@@ -1,9 +1,14 @@
 import praw
 import json
+from DataAnalysis_SS21.retrieval.Reddit.helper import print_attrs
+
+
+# Load config
+from DataAnalysis_SS21.retrieval.Reddit.stock_extraction import extract_symbols_from_submission
 
 config_path = "res/reddit_config.json"
 if __name__ == '__main__':
-    config_path = "../res/reddit_config.json"
+    config_path = "../../res/reddit_config.json"
 
 with open(config_path, "r", encoding="UTF-8") as config_file:
     config = json.load(config_file)
@@ -16,4 +21,7 @@ reddit = praw.Reddit(client_id=config["client_id"], client_secret=config["client
 wsb = reddit.subreddit("WallStreetBets")
 wsb_new = wsb.new(limit=256)
 for submission in wsb_new:
-    print(submission.score)
+    print_attrs(submission, ["title", "score", "permalink", "url", "is_video", "is_meta" "is_self", "self_text"])
+    symbols = extract_symbols_from_submission(submission)
+    print(f"Symbols:\t{symbols}")
+    print("\n\n")
