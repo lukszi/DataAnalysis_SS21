2b03ca1e7d0e48e73ee28a7430a9bb206415cfe3
Lukas Szimtenings <lszimtenings@ukaachen.de>
2021-06-22 14:01:54 +0200

    Created model representing stock mention and updated requirements

---
+++ storage/Model.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+from dataclasses_json import dataclass_json
+
+
+@dataclass_json
+@dataclass
+class StockMention:
+    post_url: str
+    stocks: list[str]
+    posted: datetime
+    votes_updated: datetime
