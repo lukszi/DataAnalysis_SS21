06f6b0d8efe09f7d355b3f3ccaa00deb354aebe5
Lukas Szimtenings <lszimtenings@ukaachen.de>
2021-06-26 14:23:21 +0200

    implemented model creation from submission

--- retrieval/reddit/redditExtractor.py
+++ retrieval/reddit/redditExtractor.py
@@ -1,9 +1,13 @@
 import json
+from datetime import datetime
+from typing import List
+
 from praw.models import Subreddit
 from praw import Reddit
 
 from DataAnalysis_SS21.retrieval.reddit.helper_methods import print_attrs
 from retrieval.reddit.symbolExtractor import SymbolExtractor
+from storage.Model import StockMention
 
 config_path = "res/reddit_config.json"
 if __name__ == '__main__':
@@ -21,14 +25,23 @@ class RedditExtractor:
         self.__setup_reddit()
         self.__symbol_extractor = SymbolExtractor("ticker.csv")
 
-    def extract_last_n_posts(self, n: int) -> None:
+    def extract_last_n_posts(self, n: int) -> List[StockMention]:
+        parsed_posts: List[StockMention] = []
         wsb_new = self.__wsb.new(limit=n)
         for submission in wsb_new:
-            print_attrs(submission,
-                        ["title", "score", "permalink", "url", "is_video", "is_meta" "is_self", "self_text"])
-            symbols = self.__symbol_extractor.extract_symbols(submission)
-            print(f"Symbols:\t{symbols}")
-            print("\n\n")
+            print_attrs(submission, ["title", "score", "permalink", "url", "is_video", "is_meta" "is_self", "self_text",
+                                     "ups", "downs", "upvote_ratio", "num_comments", "num_crossposts"])
+
+            tickers_in_submission = self.__symbol_extractor.extract_symbols(submission)
+            if len(tickers_in_submission) == 0:
+                continue
+
+            posted = datetime.fromtimestamp(submission.created_utc)
+            mention = StockMention(submission.url, tickers_in_submission, posted, datetime.now(), submission.score,
+                                   submission.ups, submission.downs, submission.upvote_ratio, submission.num_comments)
+            parsed_posts.append(mention)
+            print(f"Symbols:\t{tickers_in_submission}\n\n")
+        return parsed_posts
 
     def __load_config(self):
         with open(config_path, "r", encoding="UTF-8") as config_file:
@@ -43,5 +56,6 @@ class RedditExtractor:
 
 
 if __name__ == '__main__':
-    extr = RedditExtractor()
-    extr.extract_last_n_posts(2)
+    extractor = RedditExtractor()
+    results = extractor.extract_last_n_posts(2)
+    print(results)
